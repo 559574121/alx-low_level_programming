b12bf6ec9a23bf02f067be7e73814ced265d2e1f
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-09 15:49:54 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-print("Size of a char: 1 byte(s)\n")
-print("Size of an int: 4 byte(s)\n")
-print("Size of a long int: 4 byte(s)\n")
-print("Size of a long long int: 8 byte(s)\n")
-print("Size of a float: 4 byte(s)\n")
+printf("Size of a char: 1 byte(s)\n")
+printf("Size of an int: 4 byte(s)\n")
+printf("Size of a long int: 4 byte(s)\n")
+printf("Size of a long long int: 8 byte(s)\n")
+printf("Size of a float: 4 byte(s)\n")
 return (0);
 }
