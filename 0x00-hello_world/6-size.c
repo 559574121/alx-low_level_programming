fdf40e31b0a25df367a704e6a7d74f5b6d19783f
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-09 15:45:38 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,15 @@
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+print("Size of a char: 1 byte(s)\n")
+print("Size of an int: 4 byte(s)\n")
+print("Size of a long int: 4 byte(s)\n")
+print("Size of a long long int: 8 byte(s)\n")
+print("Size of a float: 4 byte(s)\n")
+return (0);
+}
