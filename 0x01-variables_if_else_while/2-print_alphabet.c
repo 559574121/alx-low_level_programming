ac10377f278d8fdc64186b27601ebb15082eb8bc
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-10 11:18:30 +0000

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always (Success/correct)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+
+}
