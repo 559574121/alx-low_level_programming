43f9fb0ae203337a4ced0eab93399db41985a26f
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-10 12:15:19 +0000

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always (Success/correct)
+*/
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+
+}
