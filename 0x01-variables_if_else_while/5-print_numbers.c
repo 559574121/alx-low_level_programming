c4cdd8721e475a304ffe95c1b7024049121fc20a
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-10 11:11:07 +0000

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always (Success/correct)
+*/
+int main(void)
+{
+char n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+
+}
