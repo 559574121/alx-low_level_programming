8be57cb50af5785b7a2035b68747bbf0b33c29a2
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-10 12:20:28 +0000

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always (Success/correct)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+
+}
