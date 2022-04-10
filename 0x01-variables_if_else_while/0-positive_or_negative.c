c36ee0601baefe0cf9f0676571c3f1a86c3f4e34
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-10 09:25:38 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n > 0)
 {
-printf("is positive")
+printf("is positive");
 }
 else
 if(n == 0)
 {
-printf("is zero")
+printf("is zero");
 }
 else
 {
-printf("is negative")
+printf("is negative");
 }
 return (0);
