8ceaf589c33acb2706adbcf730a128d2dc97e411
Lukeman_Dramani <Lukeman Dramani>
2022-04-12 01:51:53 -0700

    print combination4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+*main - Print 3 combination of numbers
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+int c, i, k;
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)
+{
+putchar(c);
+putchar(i);
+putchar(k);
+if (c != '7')
+{
+putchar(c);
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
