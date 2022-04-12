09912963ae83c17dceb6903bf2fba0a1eff16230
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-12 12:38:29 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,14 +15,14 @@ for (k = 48; k <= 57; k++)
 {
 for (j = 48; j <= 57; j++)
 {
-if (((k + j) > (c+i) && k >= c) || c < k)
+if (((k + j) > (c + i) && k >= c) || c < k)
 {
 putchar(c);
 putchar(i);
 putchar(' ');
 putchar(k);
 putchar(j);
-if (c + i+ k + j == 227 && c == 57)
+if (c + i + k + j == 227 && c == 57)
 {
 break;
 }
