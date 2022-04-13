32c1daabfa1715ff7bdac4ec99d488f105216413
Lukeman_Dramani <Lukeman Dramani>
2022-04-13 01:45:30 -0700

    main.h commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int_putchar(char c);
+void print_alphabet(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int_abs(int n);
+int print_last_digit(int n);
+void javk_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
