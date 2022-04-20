9697037bfd7c6af6e0ddadda4de0f5302c8ae508
Lukeman Dramani <101462605+559574121@users.noreply.github.com>
2022-04-20 16:22:32 +0000

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,30 +1,13 @@
-​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * reset_to_98 - reset argument to 98 
- ​ * @n: pointer 
- ​ * Return: void 
- ​*/ 
+​#​include​ ​"​main.h​"
+ ​/*​*
+ ​ * reset_to_98 - reset argument to 98
+ ​ * @n: pointer
+ ​ * Return: void
+ ​*/
   
- ​void​ ​reset_to_98​(​int​ *n) 
- ​{ 
-  
- ​        *n = ​98​; 
-  
- ​}
+ ​void​ ​reset_to_98​(​int​ *n)
+ ​{
 
-task 1: 1-swap.c
- 
- ​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * swap_int - swap variable values 
- ​ * @a: pointer 1 
- ​ * @b: pointer 2 
- ​ * Return: void 
- ​*/ 
+ ​        *n = ​98​;
   
- ​void​ ​swap_int​(​int​ *a, ​int​ *b) 
- ​{ 
- ​        *a += *b; 
- ​        *b = *a - *b; 
- ​        *a = *a - *b; 
  ​}
